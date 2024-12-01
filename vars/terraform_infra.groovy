f184297b9440fcca7d961862042363b69fa2a2fd
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-01 16:13:25 +0530

    infra_creation

---
+++ vars/terraform_infra.groovy
@@ -0,0 +1,21 @@
+def call (){
+    pipeline {
+        agent {
+            node {
+                label "workstation"
+            }
+        }
+
+        parameters {
+            string(name: "INFRA_ENV", default_value: "", description: "enter env like dev or prod")
+        }
+
+        stages{
+            stage('terraform init')
+            steps{
+                sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars "
+            }
+        }
+    }
+
+}
