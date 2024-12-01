482e4dd2ba6c999e08e58abd4e8f8a50361aef98
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-01 17:06:55 +0530

    ansi_color

--- vars/terraform_infra.groovy
+++ vars/terraform_infra.groovy
@@ -1,23 +1,26 @@
 def call (){
     pipeline {
-        agent {
-            node {
-                label "workstation"
-            }
+        options {
+            ansiColor('xterm')
         }
-
-        parameters {
-            string(name: 'INFRA_ENV', defaultValue: '', description: 'enter env like dev or prod')
-        }
-
-        stages{
-            stage('terraform init'){
-                steps{
-                    sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars "
+            agent {
+                node {
+                    label "workstation"
                 }
             }
 
-        }
+                parameters {
+                    string(name: 'INFRA_ENV', defaultValue: '', description: 'enter env like dev or prod')
+                }
+
+                    stages{
+                        stage('terraform init'){
+                            steps{
+                                sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+                            }
+                        }
+
+                    }
     }
 
 }
