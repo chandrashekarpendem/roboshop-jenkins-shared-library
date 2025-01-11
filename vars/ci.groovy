7e29cc325fd810792a3afb121d06c1b38b1e0e17
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2025-01-11 18:15:15 +0530

    continuous_deployment

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -47,7 +47,7 @@ def call() {
       }
 
       if(env.PUSH_CODE == "true") {
-        stage('Upload Code to Centralized Place') {
+        stage('Upload Code to Centralized Place ECR/Nexus') {
          common.artifactpush()
         }
       }
--- vars/common.groovy
+++ vars/common.groovy
@@ -12,6 +12,7 @@ def compile () {
         sh 'go build'
     }
 
+    sh "docker build -t 225989332181.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} . "
 }
 
 
@@ -35,25 +36,30 @@ def email() {
 }
 
 def artifactpush() {
-    sh "echo ${TAG_NAME} >version"
+   sh " aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 225989332181.dkr.ecr.us-east-1.amazonaws.com "
 
-    if (app_lang == "nodejs") {
-        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js version ${extra_files}"
-    }
+    sh "docker push 225989332181.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}"
 
-    if (app_lang == "nginx" || app_lang == "python") {
-        sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extra_files}"
-    }
+//    below commands are used to push Artifact to nexus as we use servers
+//    sh "echo ${TAG_NAME} >version"
 
-
-    if (app_lang == "maven")  {
-        sh "zip -r ${component}-${TAG_NAME}.zip  ${component}.jar VERSION ${extra_files}"
-    }
-    NEXUS_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-    NEXUS_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
-        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.91.211:8081/repository/${component}/${component}-${TAG_NAME}.zip"
-
-    }
+//    if (app_lang == "nodejs") {
+//        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js version ${extra_files}"
+//    }
+//
+//    if (app_lang == "nginx" || app_lang == "python") {
+//        sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extra_files}"
+//    }
+//
+//
+//    if (app_lang == "maven")  {
+//        sh "zip -r ${component}-${TAG_NAME}.zip  ${component}.jar VERSION ${extra_files}"
+//    }
+//    NEXUS_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+//    NEXUS_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+//    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
+//        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.91.211:8081/repository/${component}/${component}-${TAG_NAME}.zip"
+//
+//    }
 
 }
